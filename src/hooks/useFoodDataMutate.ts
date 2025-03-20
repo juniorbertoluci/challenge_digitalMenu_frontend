4d258b4f2ed7d37714ed44a577caa5daf4dd6c5b
Junior Bertoluci <walter.bertoluci.jr@gmail.com>
2025-03-20 08:54:45 -0300

    ASW Setings

--- src/hooks/useFoodDataMutate.ts
+++ src/hooks/useFoodDataMutate.ts
@@ -2,7 +2,7 @@ import axios, { AxiosPromise } from "axios"
 import { FoodData } from "../interface/FoodData";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'http://10.0.1.196:8080';
 
 const postData = async (data: FoodData): AxiosPromise<any> => {
     const response = axios.post(API_URL + '/food', data);
