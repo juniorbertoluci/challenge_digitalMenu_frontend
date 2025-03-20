9f22d5c48d6163588093b34b7c76d34902f2407b
Junior Bertoluci <walter.bertoluci.jr@gmail.com>
2025-03-20 09:30:57 -0300

    ASW Setings-Error

--- src/hooks/useFoodDataMutate.ts
+++ src/hooks/useFoodDataMutate.ts
@@ -2,7 +2,7 @@ import axios, { AxiosPromise } from "axios"
 import { FoodData } from "../interface/FoodData";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const API_URL = 'http://44.203.17.51/:8080';
+const API_URL = 'http://44.203.17.51:8080';
 
 const postData = async (data: FoodData): AxiosPromise<any> => {
     const response = axios.post(API_URL + '/food', data);
